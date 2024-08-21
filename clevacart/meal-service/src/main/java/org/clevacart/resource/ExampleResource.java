0cbe4b29a2c0e6534705ed735a3dc5c114813c50
OliverKeefe <oliverkeefe@tutanota.com>
2024-08-21 16:50:30 +0100

    Refactor app-service, rename to meal-service

--- clevacart/meal-service/src/main/java/org/clevacart/resource/ExampleResource.java
+++ clevacart/meal-service/src/main/java/org/clevacart/resource/ExampleResource.java
@@ -1,16 +1,16 @@
-package org.clevacart;
+package org.clevacart.resource;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/app")
+@Path("/hello")
 public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Gluten";
+        return "Hello from Quarkus REST";
     }
 }
