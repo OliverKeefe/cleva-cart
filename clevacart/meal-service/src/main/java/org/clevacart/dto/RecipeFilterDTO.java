7298a89130f3fc91cc5c68271a779f90e4e45115
OliverKeefe <oliverkeefe@tutanota.com>
2024-09-12 16:10:49 +0100

    feat: Add RecipeFilterDTO

---
+++ clevacart/meal-service/src/main/java/org/clevacart/dto/RecipeFilterDTO.java
@@ -0,0 +1,22 @@
+package org.clevacart.dto;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
+
+@ApplicationScoped
+public class RecipeFilterDTO {
+    private Integer id;
+    private String name;
+    private List<Integer> allergenIds;
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public List<Integer> getAllergenIds() { return allergenIds; }
+    public void setAllergenIds(List<Integer> allergenIds) { this.allergenIds = allergenIds; }
+
+}
